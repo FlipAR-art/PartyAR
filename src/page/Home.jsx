1dc5229b75d25db8e67d1a8d854c6254d37052e0
TomBombadilom <tom@croll.fr>
2023-07-05 17:26:56 -0300

    set FullScreen

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import loadable from "@loadable/component";
 import { useClassifier, useWebcam } from 'react-tensorflow';
 import {css} from '../layout/css';
@@ -8,9 +9,38 @@ const AudioPlayer = loadable(() => import("../audio/AudioPlayer"));
 const mode = import.meta.env.VITE_APP_MODE;
 const audioSource = import.meta.env.VITE_AUDIO_SOURCE;
 
+function toggleFullScreen(elem) {
+    // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+        if (elem.requestFullScreen) {
+            elem.requestFullScreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullScreen) {
+            elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+};
+
 const Home = () => {
   // eslint-disable-next-line no-unused-vars
   const [ videoRef, webcamTensor ] = useWebcam({ height: 224, width: 224 });
+  
+  useEffect(() => {
+    toggleFullScreen(document.body);
+  },[]);
 
   return (
     <div style={css.main}>
